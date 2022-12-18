d6af4fe6be1cb5846813f6e8735c50edd3fa9e78
Bruno Santos <devbrunosantos@gmail.com>
2022-12-18 15:49:57 -0300

    feat: checked or not with radix

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,8 +1,29 @@
-import { Trash } from "phosphor-react";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import { Check, Trash } from "phosphor-react";
+import { useState } from "react";
 
 export function Task() {
+  const [checked, setChecked] = useState(false);
+
   return (
     <div className="flex items-start justify-between gap-4 p-4 bg-gray-500 border-[1px] border-gray-400 rounded-lg shadow-lg">
+      <form>
+        <Checkbox.Root
+          className={`border-[1px] ${
+            checked === true
+              ? "bg-purple-500 border-purple-500"
+              : "border-blue-300"
+          } w-5 h-5 rounded-full flex items-center justify-center shadow-lg`}
+          onClick={() => {
+            setChecked(!checked);
+          }}
+        >
+          <Checkbox.Indicator className="text-purple-500">
+            <Check size={16} className="text-white" />
+          </Checkbox.Indicator>
+        </Checkbox.Root>
+      </form>
+
       <p className="text-gray-100 text-sm">
         Integer urna interdum massa libero auctor neque turpis turpis semper.
         Duis vel sed fames integer.
